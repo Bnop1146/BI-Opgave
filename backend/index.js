40b2d8dae0e2ed7eb3bcdec401ccddc3f2a339e2
omertekbas <tekbas425@gmail.com>
2022-11-21 22:39:43 +0100

    første del af crud

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const { data } = require('autoprefixer');
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(bodyparser.json());
 
 
 
-//database connection
+//Database Connection
  
 const db = mysql.createConnection({
     host:'localhost',
@@ -21,17 +22,17 @@ const db = mysql.createConnection({
 });
 
 
-//check database connection
+//Check Database Connection
 db.connect(err=>{
     if (err) {console.log(err,'dberr');}
     console.log('database connected ...');
 })
 
-//get all data
+//Get All Data
 
 app.get('/user', (req,res)=>{
      
-    let qr = 'select * from user';
+    let qr = `select * from user`
 
     db.query(qr,(err,result)=>{
         if (err)
@@ -52,6 +53,125 @@ app.get('/user', (req,res)=>{
 });
 
 
+
+
+//Get Single Data
+app.get('/user/:id', (req,res)=>{
+
+    let gID = req.params.id;
+
+    let qr = `select * from user where id = ${gID}`;
+
+    db.query(qr,(err,result)=>{
+
+        if(err) {console.log(err);}
+
+        if(result.length>0)
+        {
+
+            res.send({
+                message:'get single data',
+                data:result
+            });
+
+        }
+        else
+        {
+            res.send({
+                message:'data not found'
+            });
+        }
+
+
+
+    });
+
+
+});
+
+
+
+//Create Data
+
+app.post('/user',(req,res)=>{
+
+    console.log(req.body,'createdata');
+
+    let fullName = req.body.fullname;
+    let eMail = req.body.email;
+    let mb = req.body.mobile;
+
+    let qr = `insert into user(fullname, email, mobile) 
+                values('${fullName}', '${eMail}', '${mb}')`;
+
+
+
+    db.query(qr,(err,result)=>{
+
+        if(err) {console.log(err);}
+        console.log(result,'result')
+        res.send({
+            message:'data inserted'
+        });
+
+
+    });
+
+});
+
+
+
+
+//Update Data
+
+app.put('/user/:id', (req,res)=>{
+
+
+    console.log(req.body,'updatedata');
+
+    let gID = req.params.id;
+    let fullName = req.body.fullname;
+    let eMail = req.body.email;
+    let mb = req.body.mobile;
+
+    let qr = `update user set fullname = '${fullName}', email = '${eMail}', mobile = '${mb}'
+            where id = ${gID}`;
+
+
+    db.query(qr,(err,result)=>{
+
+        if(err) {console.log(err);}
+
+        res.send({
+            message:'data updated'
+        });
+
+    });
+
+
+})
+
+
+
+
+//Delete Single Data
+
+app.delete('/user/:id', (req,res)=>{
+
+    let qID = req.params.id;
+
+    let qr = `delete from user where id = '${qID}'  `;
+    db.query(qr,(err,result)=>{
+        if(err) {console.log(err);}
+
+        res.send(
+            {
+                message:'data deleted'
+            }
+        )
+    });
+
+});
 
 
 
