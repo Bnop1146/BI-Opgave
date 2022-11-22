634a27022a2d05987156260c6f239051838b1b64
omertekbas <tekbas425@gmail.com>
2022-11-22 22:43:31 +0100

    component og service tilføjet til app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'energi',
-    loadChildren: () => import('./energi/energi.module').then( m => m.EnergiPageModule)
-  },
-  {
-    path: 'vand',
-    loadChildren: () => import('./vand/vand.module').then( m => m.VandPageModule)
-  },
-  {
-    path: 'land',
-    loadChildren: () => import('./land/land.module').then( m => m.LandPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+  {path: 'home',loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'energi',loadChildren: () => import('./energi/energi.module').then( m => m.EnergiPageModule)},
+  {path: 'vand',loadChildren: () => import('./vand/vand.module').then( m => m.VandPageModule)},
+  {path: 'land',loadChildren: () => import('./land/land.module').then( m => m.LandPageModule)},
+  {path: 'login',loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
+  
+  
+  {path:'create', component:CreateComponent},
+  {path:'read', component: ReadComponent}
+
 ];
+
+
 
 @NgModule({
   imports: [
