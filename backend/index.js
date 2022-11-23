ee486ba16b4e322a324bf01e2815eec8996a8001
omertekbas <tekbas425@gmail.com>
2022-11-23 14:41:34 +0100

    autoprefixer fjernet

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql2');
-const { data } = require('autoprefixer');
 
 const app = express();
 
